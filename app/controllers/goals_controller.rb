4c69bdb18f38dcc50d40e0b385aaf308f37609cf
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-28 18:45:26 +0900

    Add goals#create

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,6 +3,20 @@ class GoalsController < ApplicationController
     @goal = Goal.new
   end
 
+  def create
+    goal = current_user.goals.new(goal_params)
+    if goal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def now
   end
+
+  private
+    def goal_params
+      params.require(:goal).permit(:title, :detail, :start, :end)
+    end
 end
