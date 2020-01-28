23ca882f4d18006a141119ebf04a064725933803
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-01-28 20:44:37 +0900

    modify_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'processes#index'
   root to: 'goals#now'
-  resources :goals, only: [:new, :create]
+  resources :goals, only: [:new, :create] do
+    resources :processes
+  end
 end
