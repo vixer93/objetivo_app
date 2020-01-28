136495fa55d25021b7e6c11d4dc20d0adb7af86c
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-28 17:16:32 +0900

    Modify migration

--- db/migrate/20200128080016_create_goals.rb
+++ db/migrate/20200128080016_create_goals.rb
@@ -1,7 +1,12 @@
 class CreateGoals < ActiveRecord::Migration[5.2]
   def change
     create_table :goals do |t|
-
+      t.references :user, foreign_key: true
+      t.string :title, null: false
+      t.date :start, null: false
+      t.date :end, null: false
+      t.string :detail
+      t.integer :status, default: 0, null: false
       t.timestamps
     end
   end
