32faf78d8ce80747f5b75ee49ca30bd520511284
shin usuda <sasasa.24.blue@gmail.com>
2020-01-31 08:42:34 +0900

    Add create function

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,24 @@
 class TodosController < ApplicationController
   def index
-    @stage = Stage.find(params[:stage_id])
+    @goal  = Goal.find(params[:goal_id])
+    @stage = @goal.stages.find(params[:stage_id])
+    @todo  = @stage.todos.new
+
     @active_todos = Todo.where(stage_id: @stage.id, status: false)
     @done_todos   = Todo.where(stage_id: @stage.id, status: true)
   end
 
   def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to goal_stage_todos_path, notice: "新規Todoを作成しました"
+    else
+      render :index
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title).merge(stage_id: params[:stage_id])
   end
 end
