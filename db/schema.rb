a143f9c7023a02f8a71f9a33cff2c48d55e9801a
atsuyakoshida <koshi0845@gmail.com>
2020-01-30 11:52:16 +0900

    Add trying to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_045315) do
+ActiveRecord::Schema.define(version: 2020_01_30_024807) do
 
   create_table "goals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_01_29_045315) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "trying", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
