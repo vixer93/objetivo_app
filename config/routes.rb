cd87c17790a6a77bcffadf9bc8009360b7bd1dd7
snskOgata <58130661+snskOgata@users.noreply.github.com>
2020-01-30 12:31:49 +0900

    Feature/goals function/change_goal_status_button (#16)
    * Add root to routes
    
    * Generate goals_controller
    
    * Add view to goals#now
    
    * Remove unneccesary files
    
    * Add style to home
    
    * Add goals#new page
    
    * Generate model
    
    * Modify migration
    
    * Migrate goals table
    
    * Add goals#new page
    
    * Add style to goals#new
    
    * Add goals#create
    
    * Generate goals_controller
    
    * Remove unneccesary files
    
    * Add style to home
    
    * Add goals#new page
    
    * Generate model
    
    * Modify migration
    
    * Migrate goals table
    
    * Add goals#new page
    
    * Add style to goals#new
    
    * Add goals#create
    
    * Remove test
    
    * Remove goal_test
    
    * Replace goal-details to DB-data
    
    * Add validation to title presence
    
    * Add change view by goal presence
    
    * Remove root_path duplication
    
    * Add routing goals#give_up
    
    * Add goals#give_up
    
    * Modify goals#give_up
    
    * Add goals#give_up button to root
    
    * Add routing to goals#pend
    
    * Add goals#pend
    
    * Add goals#pend button to root
    
    * Add style to buttons
    
    Co-authored-by: vixer93 <59346949+vixer93@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'goals#now'
   resources :goals, only: [:new, :create] do
+    patch 'pend', to: 'goals#pend'
+    patch 'give_up', to: 'goals#give_up'
     resources :stages do
       resources :todos, only: [:index, :create]
     end
