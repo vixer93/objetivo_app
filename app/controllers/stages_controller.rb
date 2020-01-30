307e2ece41687bb652482f0629a87109194032e2
Yuhei Kikuta <conan0226.yuu@gmail.com>
2020-01-30 12:47:55 +0900

    modify-view before:process after:stage

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,14 +1,27 @@
 class StagesController < ApplicationController
   def index
+    @stage = Stage.all
   end
+
   def new
+   @stage = Stage.new
   end
+
   def create
+    @stage = Stage.create(stage_params)
+    redirect_to
   end
+
   def edit
   end
   def update
   end
   def destory
   end
+
+  private
+  def stage_params
+    params.require(:stages).permit(:title, :detail, :start, :end)
+  end
+
 end
--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,2 +1,3 @@
 class Stage < ApplicationRecord
+  belongs_to :goal
 end
