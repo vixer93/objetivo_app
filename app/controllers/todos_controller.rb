0ac7f4da66b93fe7d9ad19ada0027cfbf18e9ad4
shin usuda <sasasa.24.blue@gmail.com>
2020-01-31 00:16:21 +0900

    Add index function

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,8 @@
 class TodosController < ApplicationController
   def index
+    @stage = Stage.find(params[:stage_id])
+    @active_todos = Todo.where(stage_id: @stage.id, status: false)
+    @done_todos   = Todo.where(stage_id: @stage.id, status: true)
   end
 
   def create
