34aabb2a2dbbcdd4150e85c778dc07526bdb2636
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-30 11:24:30 +0900

    Add goals#give_up

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -16,6 +16,15 @@ class GoalsController < ApplicationController
     @goal = current_user.goals.find_by(status: 0)
   end
 
+  def give_up
+    goal = Goal.find(params[:id])
+    if goal.update(status: 1)
+      redirect_to :now, notice: "#{goal.title}を諦めました"
+    else
+      redirect_to :now, notice: 'エラー：正しく処理されませんでした'
+    end
+  end
+
   private
     def goal_params
       params.require(:goal).permit(:title, :detail, :start, :end)
