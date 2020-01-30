a6ac6ffb477adfe99610cba32dd3e42ec0bb0ef5
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-30 11:23:51 +0900

    Add routing goals#give_up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'goals#now'
   resources :goals, only: [:new, :create] do
+    patch 'give_up', to: 'goals#give_up'
     resources :stages do
       resources :todos, only: [:index, :create]
     end
