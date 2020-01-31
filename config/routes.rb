d356b401ba7b21c20c9f5f99211e1cb20061401d
atsuyakoshida <koshi0845@gmail.com>
2020-01-31 19:41:48 +0900

    devise update fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
   root to: 'goals#now'
-  resources :users, only: [:edit, :update]
   resources :goals, only: [:index, :new, :create] do
     patch 'pend', to: 'goals#pend'
     patch 'give_up', to: 'goals#give_up'
