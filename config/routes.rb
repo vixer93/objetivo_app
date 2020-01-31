ee6b13ba798fc73bfae5298c9e949305d927847b
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-31 10:58:43 +0900

    Add routing for goals#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'goals#now'
-  resources :goals, only: [:new, :create] do
+  resources :goals, only: [:index, :new, :create] do
     patch 'pend', to: 'goals#pend'
     patch 'give_up', to: 'goals#give_up'
     resources :stages do
