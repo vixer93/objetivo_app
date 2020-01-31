9b8c6f8ab26442dd3271f89e09700f1bc7a95e8d
atsuyakoshida <koshi0845@gmail.com>
2020-01-31 11:47:31 +0900

    add flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'goals#now'
+  resources :users, only: [:edit, :update]
   resources :goals, only: [:new, :create] do
     patch 'pend', to: 'goals#pend'
     patch 'give_up', to: 'goals#give_up'
