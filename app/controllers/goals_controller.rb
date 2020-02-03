e37dd1407a7df4184fcc129fed7aab7c6a629def
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-02-03 17:12:54 +0900

    Complete goals#destroy

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -18,6 +18,16 @@ class GoalsController < ApplicationController
   end
 
   def destroy
+    goal = Goal.find(params[:id])
+    if goal.user_id == current_user.id
+      if goal.destroy
+        redirect_to goals_path, notice: "#{goal.title}を削除しました"
+      else
+        redirect_to goals_path, alert: goals.errors.full_messages
+      end
+    else
+      redirect_to goals_path, alert: "不正なリクエストです"
+    end
   end
 
   def now
@@ -46,7 +56,7 @@ class GoalsController < ApplicationController
   def give_up
     goal = Goal.find(params[:goal_id])
     if goal.update(status: 3)
-      redirect_to root_path, notice: "#{goal.title}を諦めました"
+      redirect_to root_path, alert: "#{goal.title}を諦めました"
     else
       redirect_to root_path, notice: 'エラー：正しく処理されませんでした'
     end
